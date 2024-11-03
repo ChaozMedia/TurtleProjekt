05c2886f2d4dbdb9e2c67c3f84181d7dd8265bf9
Julian Biederstädt <133014635+ChaozMedia@users.noreply.github.com>
2024-11-03 18:02:36 +0100

    Update turtle.lua

--- turtle.lua
+++ turtle.lua
@@ -1,11 +1,14 @@
-local modemSide = "left"  
-rednet.open(modemSide)
- 
-print("Super Krasses Turtle Skript")
-print("Computer ID: " .. os.getComputerID())
-print("--------------------------")
- 
--- Function to listen for network messages
+local function initialize()
+    local modemSide = "left"  
+    rednet.open(modemSide)
+
+    blacklist = loadBlacklist()
+    
+    print("Super Krasses Turtle Skript")
+    print("Computer ID: " .. os.getComputerID())
+    print("--------------------------")
+end
+
 local function listenForMessages()
     while true do
         local senderId, message, protocol = rednet.receive()  -- Waits for a message on any channel
@@ -16,7 +19,6 @@ local function listenForMessages()
     end
 end
  
--- Function to handle user input
 local function handleUserInput()
     while true do
         local input = read()  -- Reads from the console
@@ -36,6 +38,20 @@ local function handleUserInput()
         elseif command == "send" then
             rednet.send(tonumber(arguments[1]),arguments[2])
             print('"' ..arguments[2].. '" an ID '..arguments[1].." gesendet")  
+        elseif command == "f" then
+            if arguments[1]==nil then
+                turtle.forward()
+            else
+                for i=1,tonumber(arguments[1]) do
+                    turtle.forward()
+                end
+            end
+        elseif command == "l" then
+            turtle.turnLeft()
+        elseif command == "r" then
+            turtle.turnRight()
+        elseif command == "mine" then
+            mine()
         else
             term.setTextColor(colors.red)
             print("Kein solcher Befehl gefunden")
@@ -45,6 +61,50 @@ local function handleUserInput()
  
     end
 end
+
+local function loadBlacklist()
+    local blacklist = {}
+    local file = fs.open("blacklist.txt", "r")
+    if file then
+        local line = file.readLine()
+        while line do
+            blacklist[line] = true
+            line = file.readLine()
+        end
+        file.close()
+    end
+    return blacklist
+end
+ 
+local function isBlacklisted(blockName)
+    return (blacklist[blockName] == true)
+end
+ 
+ local function mine()
+    turtle.digDown()
+    depth = 0
+    while turtle.down() do
+        depth = depth + 1
+        turtle.digDown()
+        for i=1,4 do
+            _,a = turtle.inspect()
+            if not isBlacklisted(a["name"]) then
+                turtle.dig()
+            end
+            turtle.turnRight()
+        end
+    end
+    for i=1,depth do
+        while not turtle.up() do
+            sleep(1)
+        end
+    end
+
+ end
+ 
+
+ initialize()
+
  
 -- Run both functions in parallel
 parallel.waitForAny(listenForMessages, handleUserInput)
